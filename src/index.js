e44d338d33fc9ce578c5431544519c7b55593bc7
julian0394 <julian0394@hotmail.com>
2021-03-26 15:59:35 -0300

    tercer commit arreglando errores

--- src/index.js
+++ src/index.js
@@ -5,6 +5,3 @@ import ReactDOM from 'react-dom';
 import App from './Componentes/App';
 
 ReactDOM.render( <App /> , document.getElementById('root') );
-
-// https://www.appbrewery.co/p/web-development-course-resources/
-
