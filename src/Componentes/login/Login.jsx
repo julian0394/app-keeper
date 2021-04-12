6f0b60f029f1eec32c0ad902d9b0fce5277874a2
julian0394 <julian0394@hotmail.com>
2021-04-12 14:30:25 -0300

    estilos de input terminados

--- src/Componentes/login/Login.jsx
+++ src/Componentes/login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './login.css'
+import './login.css';
 
 function Login() {
     return (
@@ -7,15 +7,15 @@ function Login() {
             <h1>Inicia sesión</h1>
             <form action="/" method="post">
                 <div className="input-form">
-                    <p>Usuario</p>
-                    <input type="text" name="usuario" placeholder="Ingrese su usuario" required />
+                    <input type="text" name="usuario" placeholder=" " required />
+                    <label>Usuario</label>
                 </div>
                 <div className="input-form">
-                    <p>Contraseña</p>
-                    <input type="password" name="contraseña" placeholder="Ingrese su contraseña" required />
+                    <input type="password" name="contraseña" placeholder=" " required />
+                    <label>Contraseña</label>
                 </div>
                 <button type="submit">Ingresar</button>
-                <p className="registro">No estás registrado? <a href="">Registrate aquí</a></p>
+                <p className="registro">No tienes una cuenta? <a href="">Registrate aquí</a></p>
             </form>
         </div>
     )
